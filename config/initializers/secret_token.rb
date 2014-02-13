114af349e62556f1a20fabdd1f02779dea7a907b
PerfectBlue <Blue@yangxiang.gateway.2wire.net>
2014-02-12 23:09:46 -0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WarmUp::Application.config.secret_key_base = 'b9989bf99834d28159b262a9b210e3f0b4280abcc68b42a4a30fad53ce153a41eefde21b7cfcf2fc53aac752277414d8f9eed3261be6ed82acebd01a1777bd8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WarmUp::Application.config.session_store :cookie_store, key: '_warm_up_session'
