5edeadb61d7c68f0df412d96ac2d1e6f998b74a0
PerfectBlue <Blue@yangxiang.gateway.2wire.net>
2014-02-13 16:56:48 -0800

    separated unit tests

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -48,135 +48,17 @@ class UserController < ApplicationController
   end
 
   def unitTests
-    User.delete_all
-    User.new(username: "user1", password: "pass1").save
-    responseBox = ""
-    failCounter = 0
-    #Test1 Username maxsize fail
-    user = ""
-    pass = "pass2"
-    129.times do
-      user += "o"
-    end
-    temp = User.new(username: user, password: pass)
-    if temp.badUser? == User::ERR_BAD_USERNAME
-      responseBox+="Username fail-maxsize-username test: PASSED\n"
-    else
-      failcounter+=1
-      responseBox+="Username fail-maxsize-username test: FAILED\n"
-    end
+    system("RAILS_ENV=development ruby -Itest test/models/user_test.rb > test/models/test_output")
+    system("tail -1 test/models/test_output > test/models/out")
 
+    out = File.open("test/models/out")
+    value = (out.readlines())[0]
 
-    #Test2 Username is unique
-    user = "user1"
-    pass = "pass2"
-    temp = User.new(username: user, password: pass)
-    if temp.uniqueUser? == User::ERR_USER_EXISTS
-      responseBox+="Username fail-unique test: PASSED\n"
-    else
-      failcounter+=1
-      responseBox+="Username fail-unique test: FAILED\n"
-    end
+    total_tests = Integer(((value.split(",")[0]).scan(/\d+/))[0])
+    failed_tests = Integer(((value.split(",")[2]).scan(/\d+/))[0])
 
-    #Test3 Password maxsize fail
-    user = "user3"
-    pass = ""
-    129.times do
-      pass += "o"
-    end
-    temp = User.new(username: user, password: pass)
-    if temp.badPassword? == User::ERR_BAD_PASSWORD
-      responseBox+="Username fail-maxsize-password test: PASSED\n"
-    else
-      failCounter+=1
-      responseBox+="Username fail-maxsize-password test: FAILED\n"
-    end
-
-
-    #Test4 Empty Password
-    user = "user3"
-    pass = ""
-    temp = User.new(username: user, password: pass)
-    if temp.badPassword? == User::ERR_BAD_PASSWORD
-      responseBox+="Username fail-empty-password test: PASSED\n"
-    else
-      failCounter+=1
-      responseBox+="Username fail-empty-password test: FAILED\n"
-    end
-
-    #Test5 Empty Username
-    user = ""
-    pass = "pass2"
-    temp = User.new(username: user, password: pass)
-    if temp.badUser? == User::ERR_BAD_USERNAME
-      responseBox+="Username fail-empty-username test: PASSED\n"
-    else
-      failCounter+=1
-      responseBox+="Username fail-empty-username test: FAILED\n"
-    end
-
-    #Test6 Matching Credentials Fail
-    user = "user1"
-    pass = "pass3"
-    temp = User.new(username: user, password: pass)
-    if temp.credentials? == User::ERR_BAD_CREDENTIALS
-      responseBox+="Username fail-credentials test: PASSED\n"
-    else
-      failcounter+=1
-      responseBox+="Username fail-credentials test: FAILED\n"
-    end
-
-    #Test7 Passing Username
-    user = "user2"
-    pass = "pass2"
-    temp = User.new(username: user, password: pass)
-    if temp.badUser? == User::ERR_BAD_CREDENTIALS
-      failcounter+=1
-      responseBox+="Username successfull-username test: FAILED\n"
-    elsif temp.uniqueUser? == User::ERR_USER_EXISTS
-      failcounter+=1
-      responseBox+="Username successfull-username test: FAILED\n"
-    else
-      responseBox+="Username successfull-username test: PASSED\n"
-    end
-
-    #Test8 Passing Password
-    user = "user2"
-    pass = "pass2"
-    temp = User.new(username: user, password: pass)
-    if temp.badPassword? == User::ERR_BAD_PASSWORD
-      failcounter+=1
-      responseBox+="Username successfull-username test: FAILED\n"
-    else
-      responseBox+="Username successfull-credentials test: PASSED\n"
-    end
-
-    #Test9 Passing Match Credentials
-    user = "user1"
-    pass = "pass1"
-    temp = User.new(username: user, password: pass)
-    if temp.credentials? == User::ERR_BAD_CREDENTIALS
-      failcounter+=1
-      responseBox+="Username successfull-credentials test: FAILED\n"
-    else
-      responseBox+="Username successfull-credentials test: PASSED\n"
-    end
-
-    #Test10 Multiple Fails
-    user = ""
-    pass = ""
-    temp = User.new(username: user, password: pass)
-    if temp.badUser? == User::SUCCESS
-      failcounter+=1
-      responseBox+="Username fail-multi test: FAILED\n"
-    elsif temp.badPassword? == User::SUCCESS
-      failcounter+=1
-      responseBox+="Username fail-multi test: FAILED\n"
-    else
-      responseBox+="Username fail-multi test: PASSED\n"
-    end
     respond_to do |format|
-      msg = { :nrFailed => failCounter, :output => responseBox, :totalTests => 10}
+      msg = {:totalTests => total_tests, :nrFailed => failed_tests, :output => (File.open("test/models/test_output")).read}
       format.json { render json:msg}
     end
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,106 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    User.new(username: "user1", password: "pass1").save
+  end
+
+  def teardown
+    User.delete_all
+  end
+
+  #Test1 Username maxsize fail
+  test "Username_maxsize fail" do
+    user = ""
+    pass = "pass2"
+    129.times do
+      user += "o"
+    end
+    temp = User.new(username: user, password: pass)
+    assert temp.badUser? == User::ERR_BAD_USERNAME, "Username fail-maxsize-username test: FAILED\n"
+  end
+
+
+  #Test2 Username is unique
+  test "Username_unique fail" do
+    user = "user1"
+    pass = "pass2"
+    temp = User.new(username: user, password: pass)
+    assert temp.uniqueUser? == User::ERR_USER_EXISTS, "Username fail-unique test: FAILED\n"
+  end
+
+  #Test3 Password maxsize fail
+  test "Username maxsize" do
+    user = "user3"
+    pass = ""
+    129.times do
+      pass += "o"
+    end
+    temp = User.new(username: user, password: pass)
+    assert temp.badPassword? == User::ERR_BAD_PASSWORD, "Username fail-maxsize-password test: FAILED\n"
+  end
+
+
+  #Test4 Empty Password
+  test "Empty Password" do
+    user = "user3"
+    pass = ""
+    temp = User.new(username: user, password: pass)
+    assert temp.badPassword? == User::ERR_BAD_PASSWORD, "Username fail-empty-password test: FAILED\n"
+
+  end
+
+  #Test5 Empty Username
+  test "Empty Username" do
+    user = ""
+    pass = "pass2"
+    temp = User.new(username: user, password: pass)
+    assert temp.badUser? == User::ERR_BAD_USERNAME, "Username fail-empty-username test: FAILED\n"
+  end
+
+  #Test6 Matching Credentials Fail
+  test "Credential Fail" do
+    user = "user1"
+    pass = "pass3"
+    temp = User.new(username: user, password: pass)
+    assert temp.credentials? == User::ERR_BAD_CREDENTIALS, "Username fail-credentials test: FAILED\n"
+  end
+
+  #Test7 Passing Username
+  test "valid Username" do
+    user = "user2"
+    pass = "pass2"
+    temp = User.new(username: user, password: pass)
+    assert temp.badUser? == User::SUCCESS, "Username successfull-username test: FAILED\n"
+    assert temp.uniqueUser? == User::SUCCESS, "Username successfull-username test: FAILED\n"
+
+  end
+
+  #Test8 Passing Password
+  test "valid password" do
+    user = "user2"
+    pass = "pass2"
+    temp = User.new(username: user, password: pass)
+    assert temp.badPassword? == User::SUCCESS, "Username successfull-username test: FAILED\n"
+  end
+
+  #Test9 Passing Match Credentials
+  test "valid credentials" do
+    user = "user1"
+    pass = "pass1"
+    temp = User.new(username: user, password: pass)
+    assert temp.credentials? == User::SUCCESS, "Username successfull-credentials test: FAILED\n"
+
+  end
+
+  #Test10 Multiple Fails
+  test "multi_fail" do
+    user = ""
+    pass = ""
+    temp = User.new(username: user, password: pass)
+    assert temp.badUser? == User::ERR_BAD_USERNAME, "Username fail-multi test: FAILED\n"
+    assert temp.badPassword? == User::ERR_BAD_PASSWORD, "Username fail-multi test: FAILED\n"
+  end
+
 end
